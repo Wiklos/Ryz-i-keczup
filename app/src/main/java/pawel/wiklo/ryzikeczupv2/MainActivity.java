ce297450041f571360a23b136bb5cab9ab7ec12c
Wiklos <wiklos24@interia.eu>
2018-07-15 00:15:23 +0200

    Custom Action Bar with button. On click metod on this button, only toast for now.

--- app/src/main/java/pawel/wiklo/ryzikeczupv2/MainActivity.java
+++ app/src/main/java/pawel/wiklo/ryzikeczupv2/MainActivity.java
@@ -2,6 +2,9 @@ package pawel.wiklo.ryzikeczupv2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //inflate menu
+        getMenuInflater().inflate(R.menu.actionbar, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if(id==R.id.add)
+        {
+            Toast.makeText(this,"Button",Toast.LENGTH_LONG).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
