290611642777131ec4e537278757a629cc51d949
Wiklos <wiklos24@interia.eu>
2018-07-15 12:43:22 +0200

    Add Bottom navigation bar with three items. For now only template. Incoming fragments. One issue, navigation bar is not attachet to the bottom.

--- app/src/main/java/pawel/wiklo/ryzikeczupv2/MainActivity.java
+++ app/src/main/java/pawel/wiklo/ryzikeczupv2/MainActivity.java
@@ -1,5 +1,9 @@
 package pawel.wiklo.ryzikeczupv2;
 
+import android.app.Fragment;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -8,11 +12,40 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActionBar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Bottom navigation
+        toolbar = getSupportActionBar();
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        toolbar.setTitle("Initial title");
     }
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment;
+            switch (item.getItemId()) {
+                case R.id.navigation_bottom1:
+                    toolbar.setTitle("1");
+                    return true;
+                case R.id.navigation_bottom2:
+                    toolbar.setTitle("2");
+                    return true;
+                case R.id.navigation_bottom3:
+                    toolbar.setTitle("3");
+                    return true;
+            }
+            return false;
+        }
+    };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
