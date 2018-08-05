aa6bb1c2d5a08c73c3ac469dc1388afd1634e2e3
Wiklos <wiklos24@interia.eu>
2018-08-05 18:46:42 +0200

    Very first version of database. Got problem with setText in fragment, but manage to deal with it.

--- app/src/main/java/pawel/wiklo/ryzikeczupv2/active.java
+++ app/src/main/java/pawel/wiklo/ryzikeczupv2/active.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,11 +19,22 @@ public class active extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Log.e("TAG","onCreate active");
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        Log.e("TAG","Start onCreateView active");
+        //DbHelper db = new DbHelper(this);
+        DbHelper db = new DbHelper(getActivity());
+        String firstValue = "first Value";
+        String secondValue = "secondValue";
+        //db.deleteAll();
+        db.add(firstValue,secondValue);
+        db.add("11","22");
+        Log.e("TAG","End onCreateView");
         return inflater.inflate(R.layout.fragment_active, container, false);
     }
 }
--- app/src/main/java/pawel/wiklo/ryzikeczupv2/main.java
+++ app/src/main/java/pawel/wiklo/ryzikeczupv2/main.java
@@ -1,9 +1,11 @@
 package pawel.wiklo.ryzikeczupv2;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +15,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class main extends Fragment {
     public static main newInstance() {
@@ -23,11 +26,35 @@ public class main extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Log.e("TAG","onCreate main");
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        Log.e("TAG","Start onCreateView main");
+        //        DbHelper db = new DbHelper(this);
+        DbHelper db = new DbHelper(getActivity());
+
+        Cursor k = db.getAll();
+
+//        View view = inflater.inflate(R.layout.fragment_main, container, false);
+//        TextView tv = (TextView)view.findViewById(R.id.textView);
+
+        View returnView = inflater.inflate(R.layout.fragment_main, container, false);
+        TextView tv = (TextView) returnView.findViewById(R.id.textView);
+        tv.setText("hello");
+
+        tv.setText("AAAAAAAAAAAAAAAAAAA");
+        while(k.moveToNext())
+        {
+            tv.setText(tv.getText()+ "\n"+ k.getString(1) + k.getString(2) );
+            //tv.setText(k.getString(2) );
+            Log.e("TAG","Inside while k.moveToNext main");
+        }
+        Log.e("TAG","End onCreateView main");
+//        return inflater.inflate(R.layout.fragment_main, container, false);
+        return returnView;
     }
 }
